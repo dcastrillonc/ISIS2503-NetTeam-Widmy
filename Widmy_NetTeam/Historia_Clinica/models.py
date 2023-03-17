8eb242b5eda67dcabc9251ae220693037828e503
Daniel Garcia <da.garciap1@uniandes.edu.co>
2023-03-16 19:41:24 -0500

    'Historia_Clinica2.0'

--- Widmy_NetTeam/Historia_Clinica/models.py
+++ Widmy_NetTeam/Historia_Clinica/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Historia_Clinica (models.Model):
+    id_historia = models.CharField(max_length=10)
+    fechaCreacion = models.CharField(max_length=50)
+    fechaUltimaActualizacion = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=2000)
+
+    def __str__(self):
+        return '%s %s %s %s' % (self.id_historia, self.fechaCreacion, self.fechaUltimaActualizacion, self.descripcion)
