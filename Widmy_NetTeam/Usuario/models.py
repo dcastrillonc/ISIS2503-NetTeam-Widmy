6b227571ebe5ee1dd29e963d3f162ca061889350
Daniel Garcia <da.garciap1@uniandes.edu.co>
2023-03-16 19:10:21 -0500

    'Usuario2.0'

--- Widmy_NetTeam/Usuario/models.py
+++ Widmy_NetTeam/Usuario/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Usuario (models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    nombre_completo = models.CharField(max_length=50)
+    tipo_identificacion = models.CharField(max_length=50)
+    identificacion = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    rol = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '%s %s %s %s %s %s' % (self.id,self.nombre_completo, self.tipo_identificacion, self.identificacion, self.email, self.rol)
+    
+
