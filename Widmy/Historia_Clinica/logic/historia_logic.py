4dc8effd87becfb428b61dc3f87f3e0d2f0a4630
Daniela Castrillon Castro <d.castrillonc@uniandes.edu.co>
2023-05-05 21:44:24 -0500

    Views Role Historia Clinica

---
+++ Widmy/Historia_Clinica/logic/historia_logic.py
@@ -0,0 +1,9 @@
+from ..models import Historia_Clinica
+
+def get_historias():
+    queryset = Historia_Clinica.objects.all()
+    return (queryset)
+
+def get_historia(id):
+    historia = Historia_Clinica.objects.raw("SELECT * FROM historiasclinicas_historiaclinica WHERE id=%s" % id)[0]
+    return (historia)
--- Widmy/Historia_Clinica/views.py
+++ Widmy/Historia_Clinica/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from .logic.historia_logic import get_historia, get_historias
+from django.contrib.auth.decorators import login_required
+from Widmy_NetTeam.auth0backend import getRole
 
-# Create your views here.
+@login_required
+def historia_list(request):
+    role = getRole(request)
+    if role == "Administrador":
+        historias = get_historias()
+        context = {
+            'historia_list': historias
+        }
+        return render(request, context)
+    else:
+        return HttpResponse("Acceso Denegado")
+
+@login_required
+def single_historia(request, id=0):
+    historia = get_historia(id)
+    context = {
+        'historia': historia
+    }
+    return render(request, context)
